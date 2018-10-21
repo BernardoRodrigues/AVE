5aa091dfca1caf6c394751516ba9e96f6bff7477
BernardoRodrigues <bernardo.qtr.21@gmail.com>
2018-10-21 20:04:33 +0100

    added cache

---
+++ Fixtr/FixtureCache.cs
@@ -0,0 +1,21 @@
+﻿namespace Fixtr
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class FixtureCache
+    {
+
+        private static Dictionary<string, IFixture> fixtures = new Dictionary<string, IFixture>();
+
+        public static IFixture GetFixture(Type type)
+        {
+            if (fixtures.ContainsKey(type.Name))
+                return fixtures[type.Name];
+            var fixture = new FixtureReflect(type);
+            fixtures.Add(type.Name, fixture);
+            return fixture;
+        }
+
+    }
+}
